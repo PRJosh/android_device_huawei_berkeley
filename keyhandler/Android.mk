39023abea478e454d1b9626b6e441719219a4f6f
Josue Rivera <prbassplayer@gmail.com>
2018-03-30 04:08:51 +0200

    DO NOT MERGE: Disable keyhandler for now
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- keyhandler/Android.mk
+++ keyhandler/Android.mk
@@ -1,8 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 
+ifneq ($(filter disable,$(TARGET_KEYHANDLE_FIX)),)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := org.lineageos.keyhandler
 LOCAL_SRC_FILES := $(call all-java-files-under,src)
 LOCAL_MODULE_TAGS := optional
 LOCAL_DEX_PREOPT := false
 include $(BUILD_JAVA_LIBRARY)
+
+endif
